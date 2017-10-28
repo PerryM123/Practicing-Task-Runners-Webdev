6be53f1015627ccd99c6667e9edb8a4d511d86fa
perry@home <perry@inocubic.co.jp>
2017-10-28 19:16:38 +0900

    Worked on the header area. Still a lot of room for improvements... Not completed...

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,23 @@
-console.log("Main.js");
+/*
+words to learn:
+To get a value: 値を取得する
 
 
-console.log("Main.js2");
-console.log("Main.js2");
-console.log("Main.js2");
-console.log("Main.js2");
-console.log("Main.js2");
-console.log("Main.js2");
-console.log("Main.js2");
-console.log("Main.js2");
-console.log("Main.js2");
-console.log("Main.js2");
 
-//words
+*/
+
+
+var header = $( 'header .container-inner' );
+var header_offset = header.offset();
+var window_scroll_value = $(window).scrollTop();
+
+
+$(window).scroll(function() {
+	window_scroll_value = $(window).scrollTop();
+	if ( window_scroll_value >= header_offset.top ) {
+		header.addClass('fixed');
+	} 
+	else {
+		header.removeClass('fixed');
+	}
+});
