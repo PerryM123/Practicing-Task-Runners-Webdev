d21d1741f9cd9deeadd02b2068cb7ee16a3899ba
PerryM123 <perrymiranda1@yahoo.com>
2017-11-15 21:44:04 +0900

    Added JS to have there be margin added at the top of page after scrolling past the header

--- app/js/main.js
+++ app/js/main.js
@@ -5,17 +5,15 @@ To get a value: 値を取得する
 
 
 */
-
-
 var header = $( 'header .container-inner' );
+var header_height = header.height();
 var header_offset = header.offset();
 var window_scroll_value = $(window).scrollTop();
-
-
 $(window).scroll(function() {
 	window_scroll_value = $(window).scrollTop();
 	if ( window_scroll_value >= header_offset.top ) {
 		header.addClass('fixed');
+		$('body').css('margin-top', header_height + "px");
 	} 
 	else {
 		header.removeClass('fixed');
